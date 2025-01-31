4bda488ea7ea72b66201760531a60a7a44ca8cbd
blazkurent <55989335+blazkurent@users.noreply.github.com>
2025-01-31 12:53:28 +0100

    Update polysys.py

--- mosaictools/polysys.py
+++ mosaictools/polysys.py
@@ -1,5 +1,5 @@
 import numpy as np
-from distributions import UniformDistribution
+from .distributions import UniformDistribution
 from abc import ABC, abstractmethod
 
 class PolynomialSystem(ABC):
