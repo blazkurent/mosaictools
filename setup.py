945f6bdb8d9e9b4b7f6877c8f461db6ebf85ffe5
blazkurent <55989335+blazkurent@users.noreply.github.com>
2025-01-31 13:02:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mosaictools',
-    version='0.1.5',
+    version='0.1.6',
     author='Blaž Kurent',
     author_email='blaz.kurent@fgg.uni-lj.si',
     description='Surrogate modelling of modal properties using MOSAIC method',
