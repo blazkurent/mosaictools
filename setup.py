5cc4e18f3baf05651346f1af8c620191b3794633
Popovics Bence <popbence@gmail.com>
2025-06-27 22:08:48 +0200

    Version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mosaictools',
-    version='0.1.9',
+    version='0.1.10',
     author='Blaž Kurent',
     author_email='blaz.kurent@fgg.uni-lj.si',
     description='Surrogate modelling of modal properties using MOSAIC method',
