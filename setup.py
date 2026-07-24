3681f4dbd081763964ad1ce5f1837ba06a2791a4
Bence Popovics <47786942+popbence@users.noreply.github.com>
2026-07-24 17:53:30 +0200

    Exclude specific packages from find_packages

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/blazkurent/mosaictools/',
-    packages=find_packages(),
+    packages=find_packages(exclude=["mosaic_example_data", "mosaic_example_data.*", "notebooks", "notebooks.*"]),
     py_modules=['mosaictools'],
     install_requires=[
         'numpy<2.0',
