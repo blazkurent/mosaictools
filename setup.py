630fc192a493ecf345b8dd0faf131c7b44036aac
Popovics Bence <popbence@gmail.com>
2026-09-23 15:51:22 +0200

    Rename folder mosaictools to src and bump version to 0.2.7

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='mosaictools',
-    version='0.2.6',
+    version='0.2.7',
     author='Blaž Kurent, Bence Popovics',
     author_email='blaz.kurent@fgg.uni-lj.si, popbence@hun-ren.sztaki.hu',
     description='Surrogate modelling of modal properties using MOSAIC method',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/blazkurent/mosaictools/',
-    packages=find_packages(exclude=["mosaic_example_data", "mosaic_example_data.*", "notebooks", "notebooks.*"]),
-    py_modules=['mosaictools'],
+    packages=['mosaictools'] + ['mosaictools.' + p for p in find_packages(where='src', exclude=["mosaic_example_data", "mosaic_example_data.*", "notebooks", "notebooks.*"])],
+    package_dir={'mosaictools': 'src'},
     install_requires=[
         'numpy<2.0',
         'scikit-learn',
